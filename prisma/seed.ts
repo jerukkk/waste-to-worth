4eb24cc232fe17e50e8af6f52be1bd42172c4ceb
randymuflih <randymuflih@gmail.com>
2026-05-06 21:47:02 +0800

    feat: integrasi supabase database dan config prisma 7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,9 +12,15 @@
  */
 
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL || "";
+const adapter = new PrismaPg(connectionString);
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
   console.log("🌱 Starting seed...");
